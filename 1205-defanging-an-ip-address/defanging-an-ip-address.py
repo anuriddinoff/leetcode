aeff1df5b451af7ee5752e859bc3180eef5261f6
Abdulalim Nuriddinov <nuriddinoff2007@gmail.com>
2025-12-26 02:13:08 +0500

    Time: 11 ms (86.32%) | Memory: 12.4 MB (78.84%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def defangIPaddr(self, address):
+        result = ""
+        for i in address:
+            if i == '.':
+                result += '[.]'
+            else:
+                result += i
+        return result
